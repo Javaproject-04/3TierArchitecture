1f6a720a97ad165da976963558f56f083672c65a
SHUBHAM GUPTA <145351399+Javaproject-04@users.noreply.github.com>
2025-12-26 17:36:32 +0530

    Add files via upload

---
+++ aws_3tier_architecture/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'dbinstance.crus6m2iyjwe.us-west-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Admin12345',
+    DB_DATABASE : 'webappdb'
+});
